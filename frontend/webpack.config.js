1ca9c7082c2b82ccb8827d287b75e4275a76589d
Liam Morley <liam@carpeliam.com>
2016-07-19 00:26:39 -0700

    set env vars for production React/Redux deploy
    see http://stackoverflow.com/questions/30030031/passing-environment-dependent-variables-in-webpack
    [#122891911]

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -9,7 +9,8 @@ var output;
 var plugins = [
   new HtmlWebpackPlugin({ title: 'Ping Pong Registration' }),
   new webpack.DefinePlugin({
-    ACTION_CABLE_SERVER_URL: isDev ? undefined : JSON.stringify('wss://pivotal-samo-pong-reg.cfapps.io:4443/cable')
+    ACTION_CABLE_SERVER_URL: isDev ? undefined : JSON.stringify('wss://pivotal-samo-pong-reg.cfapps.io:4443/cable'),
+    'process.env.NODE_ENV': JSON.stringify(isDev ? 'development' : 'production')
   })
 ];
 
